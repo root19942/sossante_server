69093137309c41b2a194dae35522b71aa0a530aa
Rayan Kemayou <ddyayatou@gmail.com>
2019-05-28 16:58:41 +0100

    debut

---
+++ index.js
@@ -0,0 +1,40 @@
+let app = require('express')();
+
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+  });
+
+  socket.on('set-nickname', (nickname) => {
+    socket.nickname = nickname;
+    io.emit('users-changed', {user: nickname, event: 'joined'});    
+  });
+
+
+
+  socket.on('validate-sos', () => {
+    console.log("valider")
+    io.emit('consultation', {code: "693855215"});    
+  });
+  
+  socket.on('add-message', (message) => {
+    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  });
+
+
+});
+
+//var port = process.env.PORT || 3001;
+var port = 1337;
+
+// http.listen(port,"sossante", function(){
+//    console.log('listening in http://localhost:' + port);
+// });
+
+http.listen( port, "172.20.10.5", 34, function(){
+  console.log( "Server listening on port:%s", port );
+});
