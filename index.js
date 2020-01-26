73df5951f86bfea84a18e36ae5e857ff2fbe17be
root19942 <42292743+root19942@users.noreply.github.com>
2020-01-26 16:05:22 +0100

    Update index.js
    encore

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
- var express = require('express');
-var app = express();
-var path = require('path');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
+
+io.on('connection', function (socket) {
+    socket.on('onLogin', (user) => {
+      user.socket = socket.id; 
+      users[socket.id] = user; 
+      io.sockets.emit('newuser',users)
+    });
+
+
+    socket.on('onWhriting', (user_to) => {
+      for (var i = users.length - 1; i >= 0; i--) {
+        if(users[i].user.id == user_to.id){
+          user.socket = socket.id; 
+          users[socket.id] = user; 
+          io.to(user[i].socket).emit('ImOnWhriting');
+          break
+        }
+      }
+
+    });
+
+    socket.on('onSendMessage', (user_to,message) => {
+      for (var i = users.length - 1; i >= 0; i--) {
+        if(users[i].user.id == user_to.id){
+          user.socket = socket.id; 
+          users[socket.id] = user; 
+          io.to(user[i].socket).emit('IHaveSendMessage',({body:message}));
+          break
+        }
+      }
+
+    });
+
+    socket.on('disconnect', () => {
+        delete users[socket.id]
+        io.sockets.emit('userDisconnected',users)
+    });
 });
 
-app.listen(process.env.PORT || 4000, function () {
-    console.log('Node app is working!');
-});
